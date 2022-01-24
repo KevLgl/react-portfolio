178679e5eca31717a3fa947ea6c14471125db23f
Kevin Laguel <laguel.kev@gmail.com>
2022-01-24 08:05:01 +0100

    first commit:react and tailwind

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Header extends Component {
+
+  render() {
+    return(
+      <section className='bg-teal-500'>
+        <h1>test</h1>
+      </section>
+    )
+  }
+
+}
